f215d3e56333160059f79c200fe5f35eb04fe24e
Ana Virginia Campo Lemus <vickycampo@gmail.com>
2019-10-22 10:22:58 +0200

    Fin

--- src/com/rockets/java/Main.java
+++ src/com/rockets/java/Main.java
@@ -20,6 +20,7 @@ public class Main {
             //creamos el cohete 1
             propulsors1.addAll( Arrays.asList(new Integer[]{10, 30, 80} ) );
             Coets cohete1 = new Coets( identificador1 , propulsors1 );
+            cohete1.acelerar(60);
 
             cohete1.calculateRequiredPower(10000).toString();
         } catch (Exception e) {
@@ -30,6 +31,7 @@ public class Main {
             //Creamos el cohete 2
             propulsors2.addAll( Arrays.asList(new Integer[]{ 30 , 40 , 50 , 50 , 30 , 10 } ) );
             Coets cohete2 = new Coets( identificador2 , propulsors2 );
+            cohete2.acelerar(60);
 
             cohete2.calculateRequiredPower(75).toString();
         } catch (Exception e) {
